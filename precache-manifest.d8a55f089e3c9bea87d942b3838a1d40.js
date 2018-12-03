71f5c478bc45643442765a1668c13decf8de7699
mcflurrybaby <andrturko@gmail.com>
2018-12-03 13:23:06 +0300

    Updates

---
+++ precache-manifest.d8a55f089e3c9bea87d942b3838a1d40.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b9438dfb2aa480b531a",
+    "url": "/graphql-github/static/js/runtime~main.6b9438df.js"
+  },
+  {
+    "revision": "e89fd31db70aa50e1e34",
+    "url": "/graphql-github/static/js/main.e89fd31d.chunk.js"
+  },
+  {
+    "revision": "6f059a0d634b55fd8fe5",
+    "url": "/graphql-github/static/js/1.6f059a0d.chunk.js"
+  },
+  {
+    "revision": "0bbd0473b4999dc239ddfdf527221a63",
+    "url": "/graphql-github/index.html"
+  }
+];
